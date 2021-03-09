7fcf8236787c5e07137d38057b9d796c187c4635
Ben Bourgeois <1stbourgeois@gmail.com>
2021-03-09 13:01:48 -0500

    Updated main nav with jQuery controls. May need to update markup to fix completely

--- js/main.js
+++ js/main.js
@@ -6,6 +6,32 @@ $(document).ready(function(){
 
     });
 
+    var showMenuToggle = false;
+    
+    $("#menuToggleBtn").click(function(){
+
+      var menuHeight = $(window).height();
+
+      if(showMenuToggle == false) {
+
+        $(".navWrapper").animate({"height": menuHeight});
+
+        $(".navWrapper").css("background", "linear-gradient(0deg, rgba(248,249,248,0) 0%, rgba(249,248,248,1) 50%");
+
+        showMenuToggle = true;
+
+      } else {
+
+        $(".navWrapper").animate({"height": "35px"});
+        setTimeout(function() { 
+          $(".navWrapper").css("background", "transparent");
+      }, 400);
+      
+        showMenuToggle = false;
+
+      }
+    });
+
     //Flickity Listview Elements
     $('.listElement-carousel').flickity({
       cellAlign: 'left',
